eba2ba4be8c400291d115a57ab265dbd919e5f37
parui4622 <123924445+parui4622@users.noreply.github.com>
2026-09-15 21:42:38 +0530

    Fix rawKey assignment syntax in generate.js
    Updated rawKey assignment to use string syntax.

--- api/generate.js
+++ api/generate.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
     }
 
     // const rawKey = process.env.GEMINI_API_KEY;
-    const rawKey = AQ.Ab8RN6KXDRpwbZwrS6yKYHZcoWh-iQ29L7cXRQn-A20-t_YuSA;
+    const rawKey = "AQ.Ab8RN6KXDRpwbZwrS6yKYHZcoWh-iQ29L7cXRQn-A20-t_YuSA";
     const API_KEY = rawKey ? rawKey.trim() : null;
 
     if (!API_KEY) {
